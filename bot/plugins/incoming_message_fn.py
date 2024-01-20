46efcd54389be6912e5ca0aad8a67f21a6f510c4
Alireza Bime <alirezacod444@gmail.com>
2024-01-20 13:24:50 +0330

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -36,7 +36,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Usern
 #from bot.helper_funcs.utils import(
 #  delete_downloads
 #)
-os.system("wget  -O thumb.jpg")
+os.system("wget https://telegra.ph//file/744ababa5e3eeeca24372.jpg -O thumb.jpg")
 
 #LOGS_CHANNEL = -1001283278354
 CURRENT_PROCESSES = {}
